5e49bb8e518108d98736079cae53cb3b4c49901c
cutefcc <cutefcc@126.com>
2022-05-01 11:14:24 +0800

    feat: fcc-components

---
+++ fcc-components/plugin/fcc-styles.js
@@ -0,0 +1,11 @@
+const fccStyles = {
+  ":root, .theme-dark": {
+    "--sc-bgcolor": "#1b1b24",
+  },
+  ".theme-light": {
+    "--sc-bgcolor": "rgb(255, 255, 255)",
+    "--sc-primary": "#1975d1",
+    "--sc-font-primary": "#1a2027",
+  },
+};
+module.exports = fccStyles;
