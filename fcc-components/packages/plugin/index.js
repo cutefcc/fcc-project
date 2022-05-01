99ee1168cfe5d5e2ca72ebe01becdab7883ed88d
cutefcc <cutefcc@126.com>
2022-05-01 11:06:43 +0800

    feat: fcc-components

---
+++ fcc-components/packages/plugin/index.js
@@ -0,0 +1,24 @@
+// 给tailwind写插件
+const plugin = require("tailwindcss/plugin");
+const fccStyles = require("./fcc-styles");
+const fccComponents = plugin(
+  ({ addBase, addComponents, theme }) => {
+    console.log(
+      "🍺fcc-custom-tailwind-plugin启动成功🍺",
+      addBase,
+      addComponents,
+      theme
+    );
+    // 把fccStyles 给注到全局css里面
+    addComponents(fccStyles);
+  },
+  {
+    theme: {
+      color: {
+        // <div class="bg-fcc-select-hover"></div>
+        "fcc-select-hover": "var(--sc-bgcolor)",
+      },
+    },
+  }
+);
+module.exports = fccComponents;
