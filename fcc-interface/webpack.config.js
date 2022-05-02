da8ab63c487a118f5cab8973b1b6486b3aa49a01
cutefcc <cutefcc@126.com>
2022-05-02 13:54:34 +0800

    feat: fcc interface init

---
+++ fcc-interface/webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require("webpack");
+const packageName = require("./package.json").name;
+const { resolve } = require("path");
+let argv = require("yargs-parser")(process.argv.slice(2));
+const { merge } = require("webpack-merge");
+const _mode = argv.mode || "development";
+const _modeflag = _mode === "development" ? false : true;
+const _mergeConfig = require(`./config/webpack.${_mode}.js`);
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const WebpackBar = require("webpackbar");
+const webpackBaseConfig = {
+  entry: {
+    main: resolve("src/web/index.ts"),
+  },
+  output: {
+    path: resolve(process.cwd(), "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        // include: 'node_modules',
+        use: {
+          loader: "swc-loader",
+        },
+      },
+    ],
+  },
+};
+module.exports = merge.default(webpackBaseConfig, _mergeConfig);
