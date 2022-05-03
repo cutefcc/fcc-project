b79d766cf644021c780b3cc9e8b1995c90c63a9c
cutefcc <cutefcc@126.com>
2022-05-03 15:13:41 +0800

    feat: madify useAsyncFn

--- fcc-libs/packages/hooks/src/useAsyncFn.ts
+++ fcc-libs/packages/hooks/src/useAsyncFn.ts
@@ -58,6 +58,7 @@ export function useAsyncFn<T extends FuctionReturnPromise>(
       return fn(...args).then(
         value => {
           if (isMountedFn() && callId === lastCallId.current) {
+            console.log('success', value);
             setState(draft => {
               draft.loading = false;
               draft.error = undefined;
@@ -68,6 +69,7 @@ export function useAsyncFn<T extends FuctionReturnPromise>(
         },
         error => {
           if (isMountedFn() && callId === lastCallId.current) {
+            console.log('error', error);
             setState(draft => {
               draft.loading = false;
               draft.error = new Error('数据请求失败');
