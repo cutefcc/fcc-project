fe028cfeed48cec3663231711e9d2cca8dd34924
cutefcc <cutefcc@126.com>
2022-05-18 23:36:37 +0800

    feat: fcc-interface lint

--- fcc-interface/src/web/index.tsx
+++ fcc-interface/src/web/index.tsx
@@ -1,15 +1,13 @@
-import "./wdyr";
+import './wdyr';
 // import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
-import { RecoilRoot } from "recoil";
+import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 // import { fnNormalTask } from "@mmfcc/home";
 // import { useAsyncFn } from "@mmfcc/hooks";
-import App from "@pages/App";
+import App from '@pages/App';
 // import Tearing from "@pages/Tearing";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   // <StrictMode>
   <RecoilRoot>
