c143b26d7a85923068228e653a14cde458b20934
cutefcc <cutefcc@126.com>
2022-05-30 23:16:29 +0800

    fix: tearing

--- fcc-interface/src/web/components/RecoilTearing/index.tsx
+++ fcc-interface/src/web/components/RecoilTearing/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import {
   RecoilRoot,
   atom,
@@ -6,14 +6,14 @@ import {
   useRecoilState,
   useRecoilState_TRANSITION_SUPPORT_UNSTABLE,
   useRecoilValue,
-} from "recoil";
+} from 'recoil';
 const textState = atom({
-  key: "textState", // unique ID (with respect to other atoms/selectors)
-  default: "init recoil value", // default value (aka initial value)
+  key: 'textState', // unique ID (with respect to other atoms/selectors)
+  default: 'init recoil value', // default value (aka initial value)
 });
 let c = 0;
 const RecoilTearing = () => {
-  console.log("页面渲染了 - RecoilTearing", ++c);
+  console.log('页面渲染了 - RecoilTearing1', ++c);
   const [count, setCount] = useState(0);
   const [text, setText] = useRecoilState_TRANSITION_SUPPORT_UNSTABLE(textState);
   useEffect(() => {}, []);
@@ -27,9 +27,7 @@ const RecoilTearing = () => {
         <div>count:{count}</div>
         <div>text:{text}</div>
         <button onClick={() => setCount(Math.random())}>change count</button>
-        <button onClick={() => setText(Math.random().toString())}>
-          change text
-        </button>
+        <button onClick={() => setText(Math.random().toString())}>change text</button>
         test return undefined
         {/* {renderUndefined()} */}
       </div>
