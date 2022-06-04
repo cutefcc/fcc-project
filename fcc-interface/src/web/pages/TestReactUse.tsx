37fa63c5733771eee93996f3d0eb7432a8488ade
cutefcc <cutefcc@126.com>
2022-06-04 16:50:03 +0800

    fix: test useCookies

--- fcc-interface/src/web/pages/TestReactUse.tsx
+++ fcc-interface/src/web/pages/TestReactUse.tsx
@@ -2,13 +2,24 @@ import { memo, useState, useCallback } from 'react';
 import { useCookie } from '@mmfcc/hooks';
 
 function TestReactUse() {
+  const [value, updateCookie, deleteCookie] = useCookie('fcc');
   const handleSetCookie = () => {
-    useCookie('fcc', 'cutefcc');
+    updateCookie('1');
   };
+  const handleGetCookie = () => {
+    console.log('cookie fcc value:', typeof value, value);
+  };
+  const handleDeleteCookie = () => {
+    deleteCookie('fcc');
+  };
+
+  console.log('value', value);
   return (
     <div>
       <h2>TestReactUse</h2>
       <button onClick={handleSetCookie}>setCookie: key: fcc value: cutefcc</button>
+      <button onClick={handleGetCookie}>getCookie: fcc</button>
+      <button onClick={handleDeleteCookie}>deleteCookie: key: fcc </button>
     </div>
   );
 }
